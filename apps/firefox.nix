6ed719dd163c17e3663da1f0e1b07436844316c3
darkcodi <trooper982@gmail.com>
2025-12-14 17:00:25 -0600

    Add ublock origin

--- apps/firefox.nix
+++ apps/firefox.nix
@@ -1,11 +1,16 @@
-{...}: {
+{pkgs, ...}: {
   programs.firefox = {
     enable = true;
     profiles.default = {
       isDefault = true;
       settings = {
         "browser.startup.page" = 3; # restore previous session
+        "browser.theme.dark.activetab" = true;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       };
+      extensions = with pkgs.firefox-addons; [
+        ublock-origin
+      ];
     };
   };
 }
