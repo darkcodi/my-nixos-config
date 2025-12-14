b184e6f64cf431fb2f1c2a440535d8e56d1e6c87
darkcodi <trooper982@gmail.com>
2025-12-14 10:04:07 -0600

    Add oh-my-zsh

--- home.nix
+++ home.nix
@@ -36,13 +36,28 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+
     shellAliases = {
       ll = "ls -alh";
       rebuild-switch="sudo nixos-rebuild switch --flake .#misato";
     };
+
     initContent = ''
       # extra zsh config here
     '';
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "git"
+        "sudo"
+        # "docker"
+      ];
+      extraConfig = ''
+        DISABLE_AUTO_UPDATE="true"
+      '';
+    };
   };
 
   programs.direnv.enable = true;
