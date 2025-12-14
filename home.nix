90d0381e017dbe310c1c15e632e81867f1023259
darkcodi <trooper982@gmail.com>
2025-12-14 13:10:07 -0600

    Temporary comment out settings section for claude-code

--- home.nix
+++ home.nix
@@ -65,17 +65,17 @@
 
   programs.claude-code = {
     enable = true;
-    settings = {
-      env = {
-        ANTHROPIC_BASE_URL = "https://api.minimax.io/anthropic";
-        ANTHROPIC_AUTH_TOKEN = "<MINIMAX_CODING_PLAN_API_KEY>";
-        CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = 1;
-        ANTHROPIC_MODEL = "MiniMax-M2";
-        ANTHROPIC_SMALL_FAST_MODEL = "MiniMax-M2";
-        ANTHROPIC_DEFAULT_SONET_MODEL = "MiniMax-M2";
-        ANTHROPIC_DEFAULT_OPUS_MODEL = "MiniMax-M2";
-        ANTHROPIC_DEFAULT_HAIKU_MODEL = "MiniMax-M2";
-      };
-    };
+    #settings = {
+    #  env = {
+    #    ANTHROPIC_BASE_URL = "https://api.minimax.io/anthropic";
+    #    ANTHROPIC_AUTH_TOKEN = "<MINIMAX_CODING_PLAN_API_KEY>";
+    #    CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = 1;
+    #    ANTHROPIC_MODEL = "MiniMax-M2";
+    #    ANTHROPIC_SMALL_FAST_MODEL = "MiniMax-M2";
+    #    ANTHROPIC_DEFAULT_SONET_MODEL = "MiniMax-M2";
+    #    ANTHROPIC_DEFAULT_OPUS_MODEL = "MiniMax-M2";
+    #    ANTHROPIC_DEFAULT_HAIKU_MODEL = "MiniMax-M2";
+    #  };
+    #};
   };
 }
