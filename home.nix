f27471c0558d2317a3578b85bf7fddc3fc5227a6
darkcodi <trooper982@gmail.com>
2025-12-14 08:58:52 -0600

    Add firefox

--- home.nix
+++ home.nix
@@ -21,6 +21,16 @@
     };
   };
 
+  programs.firefox = {
+    enable = true;
+    profiles.default = {
+      isDefault = true;
+      settings = {
+        "browser.startup.page" = 3; # restore previous session
+      };
+    };
+  };
+
   programs.zsh.enable = true;
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
