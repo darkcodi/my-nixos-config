52ae744e810981981984067aed2e0c8ec7f101ae
Ivan Yaremenchuk <trooper982@gmail.com>
2025-12-13 20:29:44 -0600

    Enable flakes

--- configuration.nix
+++ configuration.nix
@@ -93,6 +93,9 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Enable experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
