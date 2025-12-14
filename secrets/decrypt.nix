ee7a2b36bdaf11d1b4dd05c711379aa2523387ba
darkcodi <trooper982@gmail.com>
2025-12-14 15:19:18 -0600

    Add minimax coding plan api key

--- secrets/decrypt.nix
+++ secrets/decrypt.nix
@@ -7,10 +7,9 @@ in
   # Agenix configuration for user secrets
   age.identityPaths = [ "${homeDir}/.ssh/id_ed25519" ];
   age.secrets = {
-    # Example secret: API key for sometool
-    "sometool-apikey" = {
-      file = ./sometool-apikey.age;
-      path = "${homeDir}/.sometool/apikey.txt";
+    "minimax-coding-plan-apikey" = {
+      file = ./minimax-coding-plan-apikey.age;
+      path = "${homeDir}/.minimax/apikey.txt";
       mode = "0400"; # readonly
     };
   };
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,5 +2,5 @@ let
   nixos-misato-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIErbfSfvcr9tpkG6kTJF0VI1ObdxAvBbZpnJeBH6L5vA darkcodi@misato";
 in
 {
-  "sometool-apikey.age".publicKeys = [ nixos-misato-pubkey ];
+  "minimax-coding-plan-apikey.age".publicKeys = [ nixos-misato-pubkey ];
 }
