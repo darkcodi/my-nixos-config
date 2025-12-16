671f94de25d34bc03cb2a10a40a3b762927b470e
darkcodi <trooper982@gmail.com>
2025-12-16 10:25:53 -0600

    Add misato SSH keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,4 +2,6 @@ let
   agenix-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICTPx1MpUg3RqYJJp5VmCuzTkT//y3rghBAsEas0i/VC darkcodi@misato";
 in {
   "minimax-coding-plan-apikey.age".publicKeys = [agenix-pubkey];
+  "nixos-misato-ssh-privkey.age".publicKeys = [agenix-pubkey];
+  "nixos-misato-ssh-pubkey.age".publicKeys = [agenix-pubkey];
 }
