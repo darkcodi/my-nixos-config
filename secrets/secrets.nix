888daf1e42bde48c6401b70995c8618f0b4ccbca
darkcodi <trooper982@gmail.com>
2025-12-16 08:46:54 -0600

    Rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  nixos-misato-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjHUvGLIZRAOahVtSAd55izKOjCMf1Pc0ydiML80a4j darkcodi@misato";
+  nixos-misato-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAuWgDIgg0QUdSaGikvXmyByRomwQQmrZoMuiwg6B23L darkcodi@misato";
 in {
   "minimax-coding-plan-apikey.age".publicKeys = [nixos-misato-pubkey];
 }
