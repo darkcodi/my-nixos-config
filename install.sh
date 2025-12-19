566b2a18a3e340defe96ca0edb6fd5c22aab7829
darkcodi <trooper982@gmail.com>
2025-12-19 02:16:06 -0600

    Add zram usage to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+
+# Run this script from NixOS live ISO like this:
+# $ curl -fsSL https://raw.githubusercontent.com/darkcodi/my-nixos-config/main/install.sh | bash
+
 set -euo pipefail
+
+# Add 10GB zram to prevent OOM during installation
+sudo modprobe zram
+echo $((10*1024*1024*1024)) | sudo tee /sys/block/zram0/disksize >/dev/null
+sudo mkswap /dev/zram0
+sudo swapon /dev/zram0
+
+# Disable concurrency build to reduce RAM usage
+export NIX_CONFIG=$'max-jobs = 1\ncores = 1\n'
+
+# Increase writable store tmpfs size
+sudo mount -o remount,size=10G,noatime /nix/.rw-store
 
 sudo nix --extra-experimental-features "nix-command flakes" \
   run 'github:nix-community/disko/latest#disko-install' -- \
