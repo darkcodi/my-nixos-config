bfc74b244d87f4436d9aed1602df830965f88247
darkcodi <trooper982@gmail.com>
2025-12-19 09:16:43 -0600

    Remove disko from install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,20 +5,24 @@
 
 set -euo pipefail
 
-# Add 10GB zram to prevent OOM during installation
+REPO="https://github.com/darkcodi/my-nixos-config"
+DIR="/tmp/nixos-config"
+
+# Clone the repo if does not exist
+if [ -d "$DIR/.git" ]; then
+  echo "Repo already exists at $DIR, skipping clone"
+else
+  git clone --depth 1 "$REPO" "$DIR"
+fi
+
+# Add 8GB zram to prevent OOM during installation
 sudo modprobe zram
-echo $((10*1024*1024*1024)) | sudo tee /sys/block/zram0/disksize >/dev/null
+echo $((8*1024*1024*1024)) | sudo tee /sys/block/zram0/disksize >/dev/null
 sudo mkswap /dev/zram0
 sudo swapon /dev/zram0
 
 # Disable concurrency build to reduce RAM usage
 export NIX_CONFIG=$'max-jobs = 1\ncores = 1\n'
 
-# Increase writable store tmpfs size
-sudo mount -o remount,size=10G,noatime /nix/.rw-store
-
-sudo nix --extra-experimental-features "nix-command flakes" \
-  run 'github:nix-community/disko/latest#disko-install' -- \
-  --write-efi-boot-entries \
-  --flake github:darkcodi/my-nixos-config#misato \
-  --disk main /dev/disk/by-path/pci-0000:00:17.0-ata-2
+# Perform the installation
+sudo nixos-install --root /mnt --flake $DIR#misato
