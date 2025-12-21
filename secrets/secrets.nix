78adda8465fc0ad4b42ec8d4c31047f3d32ef102
darkcodi <trooper982@gmail.com>
2025-12-21 10:06:01 -0600

    Replace comment in agenix-pubkey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  agenix-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICTPx1MpUg3RqYJJp5VmCuzTkT//y3rghBAsEas0i/VC darkcodi@misato";
+  agenix-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICTPx1MpUg3RqYJJp5VmCuzTkT//y3rghBAsEas0i/VC agenix-key";
 in {
   "minimax-coding-plan-apikey.age".publicKeys = [agenix-pubkey];
   "nixos-misato-ssh-privkey.age".publicKeys = [agenix-pubkey];
