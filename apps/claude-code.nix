b8a9c39a49d11a7da20e946646555dee3be89c38
darkcodi <trooper982@gmail.com>
2025-12-25 14:47:24 -0600

    Switch from minimax to zai

--- apps/claude-code.nix
+++ apps/claude-code.nix
@@ -5,7 +5,8 @@
 }: let
   claudeOriginal = pkgs.claude-code;
   claudeWrapped = pkgs.writeShellScriptBin "claude" ''
-    export ANTHROPIC_AUTH_TOKEN="$(cat ${config.age.secrets.minimaxCodingPlanApikey.path})"
+    #export ANTHROPIC_AUTH_TOKEN="$(cat ${config.age.secrets.minimaxCodingPlanApikey.path})"
+    export ANTHROPIC_AUTH_TOKEN="$(cat ${config.age.secrets.zaiCodingPlanApikey.path})"
     exec ${claudeOriginal}/bin/claude "$@"
   '';
 in {
@@ -17,15 +18,15 @@ in {
 
       # config without an auth token
       env = {
-        ANTHROPIC_BASE_URL = "https://api.minimax.io/anthropic";
-        # ANTHROPIC_AUTH_TOKEN = "<MINIMAX_CODING_PLAN_API_KEY>";
+        #ANTHROPIC_BASE_URL = "https://api.minimax.io/anthropic";
+        ANTHROPIC_BASE_URL = "https://api.z.ai/api/anthropic";
         API_TIMEOUT_MS = "3000000";
         CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = 1;
-        ANTHROPIC_MODEL = "MiniMax-M2.1";
-        ANTHROPIC_SMALL_FAST_MODEL = "MiniMax-M2.1";
-        ANTHROPIC_DEFAULT_SONET_MODEL = "MiniMax-M2.1";
-        ANTHROPIC_DEFAULT_OPUS_MODEL = "MiniMax-M2.1";
-        ANTHROPIC_DEFAULT_HAIKU_MODEL = "MiniMax-M2.1";
+        #ANTHROPIC_MODEL = "MiniMax-M2.1";
+        #ANTHROPIC_SMALL_FAST_MODEL = "MiniMax-M2.1";
+        #ANTHROPIC_DEFAULT_SONET_MODEL = "MiniMax-M2.1";
+        #ANTHROPIC_DEFAULT_OPUS_MODEL = "MiniMax-M2.1";
+        #ANTHROPIC_DEFAULT_HAIKU_MODEL = "MiniMax-M2.1";
         MAX_THINKING_TOKENS = "31999";
       };
     };
