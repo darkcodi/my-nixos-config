de908deccf69d169e38d93396b5431d090135741
darkcodi <trooper982@gmail.com>
2026-01-04 21:57:48 -0600

    Add one authorized key

--- hosts/misato/system.nix
+++ hosts/misato/system.nix
@@ -89,6 +89,9 @@
     extraGroups = ["networkmanager" "wheel"];
     shell = pkgs.zsh;
     packages = with pkgs; [];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE5KY8Y1PPz4f/QaiHzKeZZB6nE9cxXXiTkebVvLvmie u0_a396@localhost"
+    ];
   };
 
   # Allow unfree packages
