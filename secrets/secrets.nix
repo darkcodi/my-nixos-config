c6fd206cf8dcc2de8c28f5bde0f51c5e1bbdc072
darkcodi <trooper982@gmail.com>
2026-01-05 18:27:41 -0600

    Fix secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,10 @@
 let
   agenix-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICTPx1MpUg3RqYJJp5VmCuzTkT//y3rghBAsEas0i/VC agenix-key";
 in {
-  "minimax-coding-plan-apikey.age".publicKeys = [agenix-pubkey];
-  "nixos-misato-ssh-privkey.age".publicKeys = [agenix-pubkey];
-  "nixos-misato-ssh-pubkey.age".publicKeys = [agenix-pubkey];
-  "darkcodi-password.age".publicKeys = [agenix-pubkey];
-  "zai-coding-plan-apikey.age".publicKeys = [agenix-pubkey];
-  "tailscale-auth-key.age".publicKeys = [agenix-pubkey];
+  "age-files/minimax-coding-plan-apikey.age".publicKeys = [agenix-pubkey];
+  "age-files/nixos-misato-ssh-privkey.age".publicKeys = [agenix-pubkey];
+  "age-files/nixos-misato-ssh-pubkey.age".publicKeys = [agenix-pubkey];
+  "age-files/darkcodi-password.age".publicKeys = [agenix-pubkey];
+  "age-files/zai-coding-plan-apikey.age".publicKeys = [agenix-pubkey];
+  "age-files/tailscale-auth-key.age".publicKeys = [agenix-pubkey];
 }
