858297818171a72173e7f47aa125fe48e9324971
darkcodi <trooper982@gmail.com>
2026-01-09 17:05:38 -0600

    Update rjd to 1.2.1

--- apps/rjd.nix
+++ apps/rjd.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}: let
   src = pkgs.fetchgit {
     url = "https://github.com/darkcodi/rjd.git";
-    rev = "v1.1.0";
-    sha256 = "sha256-tsTP3SjOnaBJDqo5R/j+VscA9wve6tSElpzc2EdhsKI=";
+    rev = "v1.2.1";
+    sha256 = "sha256-vNYjtdaxriauBAq4zx1yckq/S7QOcxuHVXBDWN5z+ss=";
   };
   rjd = pkgs.rustPlatform.buildRustPackage {
     pname = "rjd";
-    version = "1.1.0";
+    version = "1.2.1";
     inherit src;
     cargoLock.lockFile = "${src}/Cargo.lock";
     cargoHash = "";
