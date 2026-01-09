4087a7cb5e03543f6e85f58db13307b754319b1b
darkcodi <trooper982@gmail.com>
2026-01-09 08:06:45 -0600

    Enable sysrq

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -2,4 +2,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 20;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable Magic SysRq keys for kernel debugging and emergency operations
+  boot.kernel.sysctl."kernel.sysrq" = 1;
 }
