327d875e567f655497721ef86931875ef892dfd6
darkcodi <trooper982@gmail.com>
2026-01-09 09:01:09 -0600

    Add dev versions of gui deps for tauri

--- apps/gui-runtime-deps.nix
+++ apps/gui-runtime-deps.nix
@@ -1,17 +1,28 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     webkitgtk_4_1
+    webkitgtk_4_1.dev
     gtk3
+    gtk3.dev
     gtk3-x11
     gdk-pixbuf
+    gdk-pixbuf.dev
     glib
+    glib.dev
     pango
+    pango.dev
     cairo
+    cairo.dev
     atk
+    atk.dev
     at-spi2-atk
+    at-spi2-atk.dev
     at-spi2-core
+    at-spi2-core.dev
     libxkbcommon
+    libxkbcommon.dev
     libepoxy
+    libepoxy.dev
     wayland
   ];
 
